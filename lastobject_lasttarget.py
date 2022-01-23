aeea4804059aa293823c85c8303cf24e350d1ddb
Alexey ALERT Rubasheff <alexey.rubasheff@gmail.com>
2022-01-23 18:51:41 +0200

    + lastobject-lasttarget basic loop macro

---
+++ lastobject_lasttarget.py
@@ -0,0 +1,24 @@
+import tools
+from Scripts.script_base import ScriptBase
+from py_stealth import *
+
+log = AddToSystemJournal
+
+debug = False
+
+
+class LastObjectLastTarget(ScriptBase):
+    def __init__(self):
+        super().__init__()
+
+    def start(self):
+        while True:
+            self.player.use_object(LastObject())
+            WaitTargetObject(LastTarget())
+
+
+if __name__ == '__main__':
+    if debug:
+        tools.debug()
+    LastObjectLastTarget().start()
+    print("")
