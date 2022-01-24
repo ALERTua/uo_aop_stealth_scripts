bc1b3f6ef58d197a0b57a43bff15806aabedfe39
Alexey ALERT Rubasheff <alexey.rubasheff@gmail.com>
2022-01-24 21:26:15 +0200

    ! tryouts prepared for testing

--- tryouts.py
+++ tryouts.py
@@ -1,11 +1,13 @@
 import sys
 import os
 import constants
-from Scripts.mining import Miner
-from Scripts.lumberjacking import Lumberjack
+from mining import Miner
+from lumberjacking import Lumberjack
+from lastobject_lasttarget import LastObjectLastTarget
 from py_stealth import *
 from player import Player
 import tools
+
 log = AddToSystemJournal
 
 debug = True
@@ -17,12 +19,6 @@ player = Player()
 
 def main():
     log('Start')
-    lj = Lumberjack(player)
-    hatchet = player.backpack_find_type(constants.TYPE_ID_HATCHET)
-    player.use_object(hatchet)
-    WaitTargetTile(0x0CD0, 2520, 249, 0)
-    # WaitForTarget(1000)
-    # TargetToTile(0x0CD0, 2520, 249, 0)
     log('Done.')
 
 
