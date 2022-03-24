ff41ca2261a0b228a2dab9df93430b7625add5fb
Alexey ALERT Rubasheff <alexey.rubasheff@gmail.com>
2022-03-24 21:00:59 +0200

    ! hiding_stealth refactoring
    ! hiding_stealth can now lastobject_lasttarget

--- hiding_stealth.py
+++ hiding_stealth.py
@@ -1,23 +1,41 @@
 from entities.base_script import ScriptBase, log, stealth, tools, constants
 
+LASTOBJECT_LASTTARGET = True
+LASTOBJECT = 0x728EF888
+LASTTARGET = 0x7356EF50
+
 
 class Hiding_Stealth(ScriptBase):
     def __init__(self):
         super().__init__()
 
+    def lo_lt(self):
+        if LASTOBJECT_LASTTARGET:
+            object_ = stealth.LastObject() if LASTOBJECT is True else LASTOBJECT
+            target = stealth.LastTarget() if LASTTARGET is True else LASTTARGET
+            if object_ and target:
+                stealth.CancelWaitTarget()
+                stealth.UseObject(object_)
+                stealth.WaitTargetObject(target)
+
     def start(self):
         super(type(self), self).start()
-        while True:
-            stealth_skill_name = 'stealth'
-            stealth_value = stealth.GetSkillValue(stealth_skill_name)
-            if stealth_value == 100.0:
-                self.quit(f'{self.player.name} {stealth_skill_name} reached {stealth_value}')
+        stealth_skill_name = 'stealth'
+        while (stealth_value := stealth.GetSkillValue(stealth_skill_name)) < 100:
+            while not self.player.hidden:
+                self.player.hide()
+                self.lo_lt()
 
-            while True:
-                self.player.hide_until_hidden()
-                while self.player.hidden:
-                    stealth.UseSkill(stealth_skill_name)
-                    tools.delay(constants.SKILL_COOLDOWN)
+            if stealth.GetSkillValue('hiding') == 100:
+                log.debug(f'Stealthing with level {stealth_value}')
+                stealth.UseSkill(stealth_skill_name)
+                self.lo_lt()
+                tools.delay(constants.SKILL_COOLDOWN)
+            else:
+                self.player.hide()
+                self.lo_lt()
+
+        self.quit(f'{self.player.name} {stealth_skill_name} reached {stealth_value}')
 
 
 if __name__ == '__main__':
